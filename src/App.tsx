f1c333dac64d2d664a994eb24b5a025fbd7ae239
Daniel <danielh839@gmail.com>
2021-07-04 18:21:28 -0400

    Added starter code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-6xl">React App</h1>
+    </div>
+  )
+}
+
+export default App;
